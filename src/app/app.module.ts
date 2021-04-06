3854cf40e6949e6f4bb49ff63886f1b9530fb6f0
Alexey Muzichenko <almuz@inbox.lv>
2021-04-06 16:44:37 +0300

    injectable in root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { FormsModule } from '@angular/forms'
-import { AppCounterService } from './services/app-counter.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    AppCounterService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,11 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
 export class AppCounterService {
-  counter = 0
+  counter = 0;
 
   increase() {
-    this.counter++
+    this.counter++;
   }
 
   decrease() {
-    this.counter--
+    this.counter--;
   }
 }
